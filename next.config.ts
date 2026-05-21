0d18b8890138623f31ea4d8f63632cc6556027a1
LauraRo166 <laura.rsanchez@mail.escuelaing.edu.co>
2026-05-20 22:06:30 -0500

    feat: add API rewrites for external endpoint in Next.js config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: 'http://tutorconnect-alb-1802029223.us-east-1.elb.amazonaws.com/api/:path*',
+            },
+        ]
+    },
 };
 
 export default nextConfig;
